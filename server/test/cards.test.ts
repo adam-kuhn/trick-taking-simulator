2272ac0d24a7be478eef1da9bae142b4472d5818
ada-kuhn <adam.kuhn20@gmail.com>
2020-11-28 12:09:26 +0200

    add card suits to assertion

--- server/test/cards.test.ts
+++ server/test/cards.test.ts
@@ -1,9 +1,17 @@
 import {expect} from 'chai'
 import { createCrewSuites } from "../controllers/cards"
 
-describe('Crew Deck', function() {
-  it('createCrewSuites generates a deck with 4 suits and 9 values for each suit', function() {
+describe('createCrewSuites', function() {
+  it('generates a deck with 4 suits and 9 cards for each suit', function() {
     const cards = createCrewSuites()
+    const greenCards = cards.filter(card => card.suit === 'green').length
+    const pinkCards = cards.filter(card => card.suit === 'pink').length
+    const blueCards = cards.filter(card => card.suit === 'blue').length
+    const yellowCards = cards.filter(card => card.suit === 'yellow').length
     expect(cards.length).to.equal(36)
+    expect(greenCards).to.equal(9)
+    expect(pinkCards).to.equal(9)
+    expect(blueCards).to.equal(9)
+    expect(yellowCards).to.equal(9)
   })
 })
