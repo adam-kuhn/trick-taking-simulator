ccff1e59c3476ff9f32fa7878538707bc88c8fa5
ada-kuhn <adam.kuhn20@gmail.com>
2020-11-28 12:03:01 +0200

    add chai assertion library

--- server/test/cards.test.ts
+++ server/test/cards.test.ts
@@ -1,9 +1,9 @@
-import assert from 'assert'
+import {expect} from 'chai'
 import { createCrewSuites } from "../controllers/cards"
 
 describe('Crew Deck', function() {
   it('createCrewSuites generates a deck with 4 suits and 9 values for each suit', function() {
     const cards = createCrewSuites()
-    assert.strictEqual(cards.length, 36)
+    expect(cards.length).to.equal(36)
   })
 })
