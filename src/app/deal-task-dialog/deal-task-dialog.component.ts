4cb55f5089328356c125fe13a34d816fb319d0de
ada-kuhn <adam.kuhn20@gmail.com>
2020-12-20 11:57:03 +0200

    add task requirment options to task config dialog - the BE should be responsible for assigning the task requirements to the tasks. Some missions allow for switching tasks. This will be the only thing allowed on the FE. It's too much have a hassle and too much work fo the user to have to assign each task requirement to each revealed task

--- src/app/deal-task-dialog/deal-task-dialog.component.ts
+++ src/app/deal-task-dialog/deal-task-dialog.component.ts
@@ -3,22 +3,45 @@ import { MatSelectChange } from '@angular/material/select';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 
 export interface TaskOptions {
-  numberOfTasks: number;
+  totalTasks: number;
+  orderedTasks: number;
+  relativeTasks: number;
   revealOnlyToCommander: boolean;
+  lastCompletedTask: boolean;
 }
+
+const numberedOptions = (totalOptions: number) => [
+  ...Array(totalOptions).keys(),
+];
+
 @Component({
   selector: 'app-deal-task-dialog',
   templateUrl: './deal-task-dialog.component.html',
+  styleUrls: ['./deal-task-dialog.component.sass'],
 })
 export class DealTaskDialogComponent {
-  options: number[] = [...Array(11).keys()];
+  options: number[] = numberedOptions(11);
+  orderSpecificOptions: number[] = numberedOptions(6);
+  relativeOptions: number[] = numberedOptions(5);
   taskRevealConfig: TaskOptions = {
-    numberOfTasks: 0,
+    totalTasks: 0,
+    orderedTasks: 0,
+    relativeTasks: 0,
+    lastCompletedTask: false,
     revealOnlyToCommander: false,
   };
 
-  setNumberOfTasks(event: MatSelectChange): void {
-    this.taskRevealConfig.numberOfTasks = event.value;
+  setTotalTasks(event: MatSelectChange): void {
+    this.taskRevealConfig.totalTasks = event.value;
+  }
+  setOrderSpecificTasks(event: MatSelectChange): void {
+    this.taskRevealConfig.orderedTasks = event.value;
+  }
+  setRelativeTasks(event: MatSelectChange): void {
+    this.taskRevealConfig.relativeTasks = event.value;
+  }
+  setLastCompletedTask(event: MatCheckboxChange): void {
+    this.taskRevealConfig.lastCompletedTask = event.checked;
   }
   setRevealToCommander(event: MatCheckboxChange): void {
     this.taskRevealConfig.revealOnlyToCommander = event.checked;
