6d0480e160092d2fe0723b0c481565e8e3ceda7f
ada-kuhn <adam.kuhn20@gmail.com>
2020-12-21 21:58:47 +0200

    display task requirements underneath cards

--- src/app/task-selection/task-selection.component.ts
+++ src/app/task-selection/task-selection.component.ts
@@ -1,6 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { Card } from '../types/game';
 
+enum SpecificOrder {
+  First = 1,
+  Second,
+  Third,
+  Fourth,
+  Fifth,
+}
+enum RelativeOrder {
+  I = 1,
+  II,
+  III,
+  IV,
+}
 @Component({
   selector: 'app-task-selection',
   templateUrl: './task-selection.component.html',
@@ -12,4 +25,11 @@ import { Card } from '../types/game';
 export class TaskSelectionComponent {
   @Input() startingTasks!: Card[];
   @Input() showTasks!: boolean;
+
+  getTaskText(e: any): string {
+    console.log(e);
+    if (e.specificOrder) return SpecificOrder[e.specificOrder];
+    if (e.relativeOrder) return `Relative-${RelativeOrder[e.relativeOrder]}`;
+    return e.lastTask ? 'Last Task' : '';
+  }
 }
