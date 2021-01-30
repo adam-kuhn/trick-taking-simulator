f9aef7d50e1d6ded337a739bff29793bac7569b7
ada-kuhn <adam.kuhn20@gmail.com>
2021-01-30 16:18:07 +0200

    passing test case for assigning a task to a player

--- src/app/task-selection/task-selection.component.spec.ts
+++ src/app/task-selection/task-selection.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatSelectChange } from '@angular/material/select';
 import { TaskSelectionComponent } from './task-selection.component';
 
 describe('TaskSelectionComponent', () => {
@@ -8,9 +8,8 @@ describe('TaskSelectionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TaskSelectionComponent ]
-    })
-    .compileComponents();
+      declarations: [TaskSelectionComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +18,17 @@ describe('TaskSelectionComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('assign selected player to the correct task', () => {
+    const selectedTask = {
+      suit: 'green',
+      value: 1,
+      player: 0,
+      completed: false,
+    };
+    component.tasks = [selectedTask];
+    const matSelectEvent = { value: 3 } as MatSelectChange;
+    const expected = { ...selectedTask, player: 3 };
+    component.setTaskToPlayer(matSelectEvent, selectedTask);
+    expect(component.tasks[0].player).toBe(expected.player);
   });
 });
