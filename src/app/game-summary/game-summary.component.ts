1ab2a9c4b8bdc02c2d5f5d34141de231b624f08a
ada-kuhn <adam.kuhn20@gmail.com>
2021-02-09 11:25:28 +0200

    create simple game summary service to hold the winning card. Update tricks won based on the winning card

--- src/app/game-summary/game-summary.component.ts
+++ src/app/game-summary/game-summary.component.ts
@@ -1,20 +1,51 @@
-import { Component, Input } from '@angular/core';
-import { GameService } from '../services/game.service';
+import { Component, Input, OnChanges } from '@angular/core';
+import { GameSummaryService } from '../services/game-summary.service';
 import { Communication, PlayerCard } from '../types/game';
 
 @Component({
   selector: 'app-game-summary',
   templateUrl: './game-summary.component.html',
   styleUrls: ['./game-summary.component.sass'],
-  providers: [GameService],
 })
-export class GameSummaryComponent {
+export class GameSummaryComponent implements OnChanges {
   @Input() numberOfPlayers!: number;
   @Input() isPlayerCommander!: boolean;
-  @Input() winningCard!: PlayerCard | null;
   @Input() leadCard!: PlayerCard | null;
   @Input() lastTrick!: PlayerCard[];
   @Input() revealedCommunications!: Communication[];
+
+  winningCard: PlayerCard | null = null;
+  wonTrickSummary: { player: number; tricks: number }[] = [];
+
+  constructor(private gameSummaryService: GameSummaryService) {
+    this.gameSummaryService.winningCard$.subscribe((card) => {
+      this.winningCard = card;
+      if (!this.winningCard) return;
+      const winnerOfLastTrick = this.winningCard.player;
+      const playerSummary = this.wonTrickSummary.find(
+        (summary) => summary.player === winnerOfLastTrick
+      );
+      if (!playerSummary) return;
+      playerSummary.tricks++;
+    });
+  }
+
+  ngOnChanges(): void {
+    if (this.wonTrickSummary.length !== this.numberOfPlayers) {
+      this.createTrickSummary();
+    }
+  }
+
+  createTrickSummary(): void {
+    this.wonTrickSummary = [...Array(this.numberOfPlayers).keys()].map(
+      (player) => {
+        return {
+          player: player + 1,
+          tricks: 0,
+        };
+      }
+    );
+  }
 
   formatInformation(card: PlayerCard, communicationType?: string): string {
     const playersCard = `Player ${card.player}`;
