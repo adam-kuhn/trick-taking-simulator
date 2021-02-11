72a5d1b74c46703eff7e5ef10d4c92451c4586b7
ada-kuhn <adam.kuhn20@gmail.com>
2021-02-11 16:22:55 +0200

    remove the / from url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backEndUrl: 'https://trick-tacking-simulator.herokuapp.com/',
+  backEndUrl: 'https://trick-tacking-simulator.herokuapp.com',
 };
