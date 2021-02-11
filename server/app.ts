e2d9b53e1c09714c4dcedb5f63e7efa20df8f636
ada-kuhn <adam.kuhn20@gmail.com>
2021-02-11 13:04:59 +0200

    complete set up for deployment...

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 import { createServer } from 'http';
 import { Server, Socket } from 'socket.io';
@@ -8,7 +9,10 @@ const app = express();
 const httpServer = createServer(app);
 
 const cors = {
-  origin: 'http://localhost:4200',
+  origin:
+    process.env.NODE_ENV === 'prod'
+      ? 'https://trick-tacking-simulator.herokuapp.com/'
+      : 'http://localhost:3000',
   methods: ['GET'],
 };
 const io = new Server(httpServer, {
@@ -31,6 +35,18 @@ app.get('/connections', (req, res) => {
   } catch (error) {
     res.status(500).send('Could not access live connections');
   }
+});
+
+app.get('*', (req, res) => {
+  let file: string = req.path.slice(1);
+  if (file === '') {
+    file = 'index.html';
+  }
+  const filePath =
+    process.env.NODE_ENV === 'dev'
+      ? '../dist/trick-taking-simulator'
+      : '../../trick-taking-simulator';
+  res.sendFile(path.join(__dirname, `${filePath}/${file}`));
 });
 
 httpServer.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  backEndUrl: 'https://trick-tacking-simulator.herokuapp.com/',
 };
