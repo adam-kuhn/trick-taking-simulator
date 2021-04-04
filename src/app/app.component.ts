1758491daffbfd3b15c735468b87a085baaecc6c
ada-kuhn <adam.kuhn20@gmail.com>
2021-04-04 09:23:45 +0300

    require simple game code to join

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,11 +17,14 @@ export class AppComponent implements OnInit {
   // until connections request comes back
   connectedClients = 5;
   requestConnections = 0;
+  gameCode = '';
+  private validGameCode = '';
 
   async ngOnInit(): Promise<void> {
     this.requestConnections = window.setInterval(async () => {
       await this.getConnections();
     }, 1000);
+    this.createGameCode();
   }
 
   async getConnections(): Promise<void> {
@@ -34,7 +37,23 @@ export class AppComponent implements OnInit {
       console.error('Oops, something went wrong!', error.message);
     }
   }
-
+  /* stupid simple password to enter the game this is just 
+  to prevent randoms for joining the game and is not meant to be secure
+  TODO: make game individual game rooms */
+  createGameCode(): void {
+    const now = new Date();
+    const month = now.getMonth() + 1;
+    const date = now.getDate();
+    this.validGameCode = `${date}-${month}_going-to-space`;
+    // log the code in the console, so I don't have to remember the password :)
+    console.log('Your game code is:', this.validGameCode);
+  }
+  setGameCode(event: Event): void {
+    this.gameCode = (event?.target as HTMLInputElement).value;
+  }
+  isGameCodeValid(): boolean {
+    return this.gameCode === this.validGameCode;
+  }
   joinGame(): void {
     this.inGame = true;
     window.clearInterval(this.requestConnections);
