9ace6ed1e1a88d751c8ee941bb4efcd5ef78fa56
ada-kuhn <adam.kuhn20@gmail.com>
2021-04-05 09:52:01 +0300

    add validation and requirements to join game dialog

--- src/app/join-game-dialog/join-game-dialog.component.ts
+++ src/app/join-game-dialog/join-game-dialog.component.ts
@@ -1,35 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  AbstractControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+const createGameCode = (): string => {
+  /* stupid simple password to enter the game this is just 
+  to prevent randoms for joining the game and is not meant to be secure
+  TODO: make game individual game rooms */
+  const now = new Date();
+  const month = now.getMonth() + 1;
+  const date = now.getDate();
+  const gameCode = `${date}-${month}_going-to-space`;
+  // log the code in the console, so I don't have to remember the password :)
+  console.log('Your game code is:', gameCode);
+  return gameCode;
+};
 
 @Component({
   selector: 'app-join-game-dialog',
   templateUrl: './join-game-dialog.component.html',
   styleUrls: ['./join-game-dialog.component.sass'],
 })
-export class JoinGameDialogComponent implements OnInit {
+export class JoinGameDialogComponent {
   gameInfo = new FormGroup({
-    gameCode: new FormControl(''),
-    username: new FormControl(''),
+    gameCode: new FormControl('', [
+      Validators.required,
+      Validators.pattern(createGameCode()),
+    ]),
+    username: new FormControl('', [
+      Validators.maxLength(15),
+      Validators.pattern('[a-zA-Z0-9]*'),
+    ]),
   });
   validGameCode = '';
 
-  ngOnInit(): void {
-    this.createGameCode();
+  get gameCode(): AbstractControl | null {
+    return this.gameInfo.get('gameCode');
   }
 
-  /* stupid simple password to enter the game this is just 
-  to prevent randoms for joining the game and is not meant to be secure
-  TODO: make game individual game rooms */
-  createGameCode(): void {
-    const now = new Date();
-    const month = now.getMonth() + 1;
-    const date = now.getDate();
-    this.validGameCode = `${date}-${month}_going-to-space`;
-    // log the code in the console, so I don't have to remember the password :)
-    console.log('Your game code is:', this.validGameCode);
-  }
-  isGameCodeValid(): boolean {
-    console.log('val', this.gameInfo.value);
-    return this.gameInfo.value.gameCode === this.validGameCode;
+  get username(): AbstractControl | null {
+    return this.gameInfo.get('username');
   }
 }
