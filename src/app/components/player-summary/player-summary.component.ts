9dc07722925510e91810b2f2ae807cf0f925256a
ada-kuhn <adam.kuhn20@gmail.com>
2021-06-24 11:07:53 +0300

    Clear player task-list when new tasks are dealt

--- src/app/components/player-summary/player-summary.component.ts
+++ src/app/components/player-summary/player-summary.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { SocketService } from '../../services/socket.service';
 import { handleCardDropEvent } from '../../utils/card-dragging';
 import { TaskCard, Player } from '../../types/game';
 import { PlayerDisplayNamePipe } from '../../pipes/player-display-name/player-display-name.pipe';
@@ -12,7 +13,14 @@ import { PlayerDisplayNamePipe } from '../../pipes/player-display-name/player-di
 export class PlayerSummaryComponent {
   @Input() playerInfo!: Player | undefined;
   taskCards: TaskCard[] = [];
-  constructor(private playerDisplayName: PlayerDisplayNamePipe) {}
+  constructor(
+    private socketService: SocketService,
+    private playerDisplayName: PlayerDisplayNamePipe
+  ) {
+    this.socketService.recieveTaskCards().subscribe(() => {
+      this.taskCards = [];
+    });
+  }
 
   playersTableText(): string {
     if (!this.playerInfo) return '';
