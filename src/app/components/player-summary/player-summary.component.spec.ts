e81afc2a0d5d4d24853d0175f7b103035f046abd
adam-kuhn <adam.kuhn@smartly.io>
2021-06-24 15:53:22 +0300

    Add pipes to get initial test to pass

--- src/app/components/player-summary/player-summary.component.spec.ts
+++ src/app/components/player-summary/player-summary.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { PlayerDisplayNamePipe } from '../../pipes/player-display-name/player-display-name.pipe';
+import { PlayerTaskListPipe } from '../../pipes/player-task-list/player-task-list.pipe';
 import { PlayerSummaryComponent } from './player-summary.component';
 
 describe('PlayerSummaryComponent', () => {
@@ -8,9 +9,9 @@ describe('PlayerSummaryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PlayerSummaryComponent ]
-    })
-    .compileComponents();
+      declarations: [PlayerSummaryComponent, PlayerTaskListPipe],
+      providers: [PlayerDisplayNamePipe, PlayerTaskListPipe],
+    }).compileComponents();
   });
 
   beforeEach(() => {
