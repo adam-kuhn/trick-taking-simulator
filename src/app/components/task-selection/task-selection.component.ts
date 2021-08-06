8a9f464b44e33788a51569e5627dc8432944fbb6
adam-kuhn <adam.kuhn@smartly.io>
2021-08-06 10:04:11 +0300

    Enable swapping the ordering of tasks

--- src/app/components/task-selection/task-selection.component.ts
+++ src/app/components/task-selection/task-selection.component.ts
@@ -1,7 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { InitialTasks, TaskCard, Player } from '../../types/game';
 import { SocketService } from '../../services/socket.service';
 import { PlayerTaskListPipe } from '../../pipes/player-task-list/player-task-list.pipe';
+import {
+  ConfirmDialogComponent,
+  DialogActions,
+} from '../confirm-dialog/confirm-dialog.component';
 @Component({
   selector: 'app-task-selection',
   templateUrl: './task-selection.component.html',
@@ -13,10 +18,12 @@ export class TaskSelectionComponent {
   tasks: TaskCard[] = [];
   showTasks = true;
   revealOnlyToCommander = false;
+  tasksToEdit: TaskCard[] = [];
 
   constructor(
     private socketService: SocketService,
-    private playerTaskList: PlayerTaskListPipe
+    private playerTaskList: PlayerTaskListPipe,
+    private dialog: MatDialog
   ) {
     this.socketService.recieveAssignedTask().subscribe((data: TaskCard) => {
       this.tasks = this.tasks.filter(
@@ -45,5 +52,69 @@ export class TaskSelectionComponent {
     return this.playerSummary.map((player) =>
       this.playerTaskList.transform(player)
     );
+  }
+  handleTaskSelect(task: TaskCard): void {
+    if (
+      this.tasksToEdit.length === 1 &&
+      this.tasksToEdit[0].suit !== task.suit &&
+      this.tasksToEdit[0].value !== task.value
+    ) {
+      this.tasksToEdit.push(task);
+    } else {
+      this.tasksToEdit.push(task);
+    }
+    this.swapTaskOrder();
+  }
+  swapTaskOrder(): void {
+    if (this.tasksToEdit.length !== 2) return;
+    const data = {
+      message: 'Swap the order requirement of these two tasks?',
+      actions: DialogActions.CONFIRM,
+      cards: this.tasksToEdit,
+    };
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, { data });
+    dialogRef.afterClosed().subscribe((confirmation: string) => {
+      if (confirmation === 'confirm') {
+        const [taskOne, taskTwo] = this.tasksToEdit;
+        const taskOneOrder = this.getTaskOrder(taskOne);
+        const taskTwoOrder = this.getTaskOrder(taskTwo);
+        this.updateTaskInplace(
+          taskOne,
+          this.updateTaskCardOrdering(taskOne, taskTwoOrder)
+        );
+        this.updateTaskInplace(
+          taskTwo,
+          this.updateTaskCardOrdering(taskTwo, taskOneOrder)
+        );
+      }
+      this.tasksToEdit = [];
+    });
+  }
+  getTaskOrder(
+    task: TaskCard
+  ): { relativeOrder?: number; specificOrder?: number; lastTask?: boolean } {
+    const { relativeOrder, specificOrder, lastTask } = task;
+    if (relativeOrder) return { relativeOrder };
+    if (specificOrder) return { specificOrder };
+    if (lastTask) return { lastTask };
+    return {};
+  }
+  updateTaskCardOrdering(
+    task: TaskCard,
+    ordering: Partial<TaskCard>
+  ): TaskCard {
+    return {
+      suit: task.suit,
+      value: task.value,
+      playerPosition: task.playerPosition,
+      username: task.username,
+      completed: task.completed,
+      ...ordering,
+    };
+  }
+
+  updateTaskInplace(task: TaskCard, updatedTask: TaskCard): void {
+    const taskIndex = this.tasks.indexOf(task);
+    this.tasks.splice(taskIndex, 1, updatedTask);
   }
 }
