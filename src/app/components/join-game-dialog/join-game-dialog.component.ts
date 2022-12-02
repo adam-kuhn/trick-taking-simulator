89d4eadb0fc12355e2725ebda6de2db4a3a4f766
adam-kuhn <adam.kuhn@smartly.io>
2022-12-02 16:40:31 +0200

    Add room code validation to join dialog - tests need to be updated

--- src/app/components/join-game-dialog/join-game-dialog.component.ts
+++ src/app/components/join-game-dialog/join-game-dialog.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import {
   FormControl,
   AbstractControl,
   FormGroup,
   Validators,
+  ValidatorFn,
+  FormBuilder,
 } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 export const createGameCode = (): string => {
-  /* stupid simple password to enter the game this is just 
+  /* stupid simple password to enter the game this is just
   to prevent randoms for joining the game and is not meant to be secure
   TODO: make individual game rooms */
   const now = new Date();
@@ -25,23 +28,50 @@ export const createGameCode = (): string => {
   styleUrls: ['../shared-styles/dialog.sass'],
 })
 export class JoinGameDialogComponent {
-  gameInfo = new FormGroup({
-    gameCode: new FormControl('', [
-      Validators.required,
-      Validators.pattern(createGameCode()),
-    ]),
-    username: new FormControl('', [
-      Validators.maxLength(15),
-      Validators.pattern('(?=[^a-zA-Z0-9]*[a-zA-Z0-9])[a-zA-Z0-9\\s]*'),
-    ]),
-  });
-  validGameCode = '';
+  rooms: Record<string, string> = {};
+  gameInfo: FormGroup;
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: { rooms: Record<string, string> },
+    private formBuilder: FormBuilder
+  ) {
+    this.rooms = data.rooms;
+    this.gameInfo = this.formBuilder.group({
+      roomCredentials: this.formBuilder.group(
+        {
+          gameRoom: new FormControl('', Validators.required),
+          gameCode: new FormControl('', [Validators.required]),
+        },
+        { validators: this.myvalidate }
+      ),
+      username: new FormControl('', [
+        Validators.maxLength(15),
+        Validators.pattern('(?=[^a-zA-Z0-9]*[a-zA-Z0-9])[a-zA-Z0-9\\s]*'),
+      ]),
+    });
+  }
+
+  get roomNames(): string[] {
+    return Object.keys(this.rooms);
+  }
 
   get gameCode(): AbstractControl | null {
-    return this.gameInfo.get('gameCode');
+    return this.gameInfo.get('roomCredentials.gameCode');
+  }
+
+  get roomCredentials(): AbstractControl | null {
+    return this.gameInfo.get('roomCredentials');
   }
 
   get username(): AbstractControl | null {
     return this.gameInfo.get('username');
   }
+  myvalidate: ValidatorFn = (formGroup: AbstractControl) => {
+    const formCode = formGroup.get('gameCode')?.value;
+    const formName = formGroup.get('gameRoom')?.value;
+    if (this.rooms[formName] === formCode) {
+      return null;
+    }
+    return { invalidCode: true };
+  };
 }
